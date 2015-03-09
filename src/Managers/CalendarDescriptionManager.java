d7dc73415af7b026c2e92456402c99e69e6f8ff2
fRandOmizer <zan.richard@gmail.com>
2015-03-09 19:06:28 +0100

    Fixed

--- src/Managers/CalendarDescriptionManager.java
+++ src/Managers/CalendarDescriptionManager.java
@@ -6,12 +6,15 @@ import Interfaces.CalendarDescriptionManagerInterface;
  * Created by Richard on 8. 3. 2015.
  */
 public class CalendarDescriptionManager implements CalendarDescriptionManagerInterface {
+    @Override
     public void CreateDescription(String text){
 
     }
+    @Override
     public void EditDescription(String text){
 
     }
+    @Override
     public String GetDescription()
     {
         return null;
--- src/Managers/DayManager.java
+++ src/Managers/DayManager.java
@@ -9,15 +9,19 @@ import java.util.Date;
  * Created by Richard on 8. 3. 2015.
  */
 public class DayManager implements DayManagerInterface {
+    @Override
     public void CreateDay(Date date){
 
     }
+    @Override
     public void EditDay(Day day){
 
     }
+    @Override
     public void DeleteDay(Day day){
 
     }
+    @Override
     public Day FindByDay(Date day){
         return  null;
     }
